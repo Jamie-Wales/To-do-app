24b57b25ba24b72d765d4cba4d6556b32cd6a679
Jamie Wales <jamie.wales.developer@gmail.com>
2021-01-03 15:30:15 +0000

    general layout styling before adding features to JS

--- app.js
+++ app.js
@@ -6,24 +6,37 @@ document.addEventListener("DOMContentLoaded", () => {
    
     button.addEventListener("click", (e) => {
         var input = document.getElementById("to-do-sub").value;
-        inputArray.push(input)
-        if (inputArray.length > 0) {
-            for (let i = 0; i < inputArray.length; i++) {
-                let newEl = document.createElement('li');
-                let newText = document.createTextNode(inputArray[i]);
-                newEl.appendChild(newText);
-                let position = document.getElementById('to-do-list');
-                position.appendChild(newEl);
-                
-                
-            
-            }
+        if (input.length == 0){
+            alert('Please enter valid input')
+        } else {
+            inputArray.unshift(input)
+            let newEl = document.createElement('li');
+            let newText = document.createTextNode(inputArray[0]);
+            newEl.appendChild(newText);
+            let position = document.getElementById('to-do-list');
+            position.appendChild(newEl);
+            newEl.classList.add('element');
+            let doneButton = document.createElement("button");
+            let removeButton = document.createElement("button");
+            newEl.appendChild(doneButton);
+            newEl.appendChild(removeButton);
+            doneButton.classList.add('doneButton');
+            removeButton.classList.add('removeButton');
         }
+        
+    
+       
+        
+        
         
         
         
       
     })
+
+    
        
+
+    
 
 })
